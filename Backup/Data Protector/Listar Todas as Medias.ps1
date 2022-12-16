219af6809c0b49aa23a83f157bedc65e1e7f958e
Diego Voltolini | ic3n3t <diegovolt@gmail.com>
2022-12-16 15:07:02 -0300

    Update Listar Todas as Medias.ps1

--- Backup/Data Protector/Listar Todas as Medias.ps1
+++ Backup/Data Protector/Listar Todas as Medias.ps1
@@ -1,5 +1,5 @@
-No powershell para exibir em tela:
+## No powershell para exibir em tela: ##
 omnirpt -report media_list
 
-No powershell, navegar até o diretório para exportar em txt, usar o seguinte comando:
+## No powershell, navegar até o diretório para exportar em txt, usar o seguinte comando: ##
 omnirpt -report media_list > medialist.txt
